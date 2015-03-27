9e37ad6cfb2b20b1d6833f4278e51c9d5003022f
Muhaimin Ahsan <leroux@fezrev.com>
2015-03-26 22:15:58 -0500

    sudo upfront and keep-alive.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,6 +2,11 @@
 
 BATTLESTATION_POWERUP_DIR="/tmp/battlestation-powerup-master"
 
+# Here we go.. ask for the administrator password upfront and run a
+# keep-alive to update existing `sudo` time stamp until script has finished
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 if [ ! -d "$BATTLESTATION_POWERUP_DIR" ]; then
 	echo -e  "\n\033[0;32m>>> Download battlestation-powerup\033[0m"
 	if [ -d $BATTLESTATION_POWERUP_DIR ]; then
