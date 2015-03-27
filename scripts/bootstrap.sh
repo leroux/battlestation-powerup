d660c924fc73ef1f6d1b0deed77c8422235d53ee
Muhaimin Ahsan <leroux@fezrev.com>
2015-03-26 22:04:11 -0500

    More.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,13 +3,12 @@
 BATTLESTATION_POWERUP_DIR="/tmp/battlestation-powerup"
 
 if [ ! -d "$BATTLESTATION_POWERUP_DIR" ]; then
-	echo -e  "\n\033[0;32m >> Download battlestation-powerup\033[0m"
+	echo -e  "\n\033[0;32m>>> Download battlestation-powerup\033[0m"
 	if [ -d $BATTLESTATION_POWERUP_DIR ]; then
 		rm -rf $BATTLESTATION_POWERUP_DIR
 	fi
-	wget https://github.com/leroux/battlestation-powerup/archive/master.zip \
-		-O /tmp/battlestation-powerup.zip
-	unzip /tmp/battlestation-powerup.zip -d /tmp
+	curl -o "$BATTLESTATION_POWERUP_DIR.zip" https://github.com/leroux/battlestation-powerup/archive/master.zip
+	unzip "$BATTLESTATION_POWERUP_DIR.zip" -d /tmp
 fi
 
 cd $BATTLESTATION_POWERUP_DIR
